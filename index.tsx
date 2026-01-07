b7ab393b85693ad0b7f986ee6973111bc218f5b9
ash <ash>
2026-01-07 17:27:34 +0530

    updates zintle apk

--- index.tsx
+++ index.tsx
@@ -503,7 +503,7 @@ const Hero = ({ setShowCoins }: { setShowCoins: (v: boolean) => void }) => (
             style={{ animationDelay: "0.2s" }}
           >
             <a
-              href="https://biffle-apk.s3.ap-south-1.amazonaws.com/zintel.apk"
+              href="https://biffle-apk.s3.ap-south-1.amazonaws.com/zintle_v2.apk"
               download
               className="w-full sm:w-auto bg-transparent border border-white/20 text-white font-bold py-4 px-8 rounded-full hover:bg-white/5 transition-all flex items-center justify-center gap-3 transform hover:scale-105"
             >
